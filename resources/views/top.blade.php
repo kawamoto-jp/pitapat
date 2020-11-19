2d411832b82ab0482283471b8f25722d85b763c3
kawamoto-jp <k400345@gmail.com>
2020-11-19 19:49:20 +0900

    Initial commit

---
+++ resources/views/top.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Top</title>
+</head>
+<body>
+  <header>
+    <h1>PitaPat</h1>
+    <div class="user_management">
+      <p class="signin"><a href="#">新規登録</a></p>
+      <p class="login"><a href="#">ログイン</a></p>
+    </div>
+  </header>
+</body>
+</html>
