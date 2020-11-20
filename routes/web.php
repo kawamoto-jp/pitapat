bb816d3c341840324b3933175fa322d4dc04144f
kawamoto-jp <k400345@gmail.com>
2020-11-21 02:20:20 +0900

    完成

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//管理側
+Route::group(['middleware' => ['auth.admin']], function () {
+
+  //管理側トップ
+  Route::get('/admin', 'App\Http\Controllers\admin\AdminTopController@show');
+  //ログアウト実行
+  Route::post('/admin/logout', 'App\Http\Controllers\admin\AdminLogoutController@logout'); 
+  //ユーザー一覧
+  Route::get('/admin/user_list', 'App\Http\Controllers\admin\ManageUserController@showUserList');
+  //ユーザー詳細
+  Route::get('/admin/user/{id}', 'App\Http\Controllers\admin\ManageUserController@showUserDetail');
+
+});
+
+//管理側ログイン
+Route::get('/admin/login', 'App\Http\Controllers\admin\AdminLoginController@showLoginform');
+Route::post('/admin/login', 'App\Http\Controllers\admin\AdminLoginController@login');
